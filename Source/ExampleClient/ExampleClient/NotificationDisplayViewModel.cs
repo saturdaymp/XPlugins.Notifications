48e82a5e240f39ffefed0b24fd520d4ac1f50e7a
Chris C <chris.cumming@saturdaymp.com>
2016-11-16 17:20:00 -0700

    Missed these files from the last checkin.  Code formatting.

--- Source/ExampleClient/ExampleClient/NotificationDisplayViewModel.cs
+++ Source/ExampleClient/ExampleClient/NotificationDisplayViewModel.cs
@@ -1,13 +1,31 @@
 ﻿namespace ExampleClient
 {
+    /// <summary>
+    ///     View model for the Notification Display page.  Has
+    ///     values parsed from the notification.
+    /// </summary>
     public class NotificationDisplayViewModel
     {
+        /// <summary>
+        ///     Title of the notification.
+        /// </summary>
         public string Title { get; set; }
 
+        /// <summary>
+        ///     The message in the notification.
+        /// </summary>
         public string Message { get; set; }
 
-        public int ExtraInfoNumber { get; set; }
+        /// <summary>
+        ///     The numberic extra info in the notification.  If not
+        ///     supplied then will be null.
+        /// </summary>
+        public int? ExtraInfoNumber { get; set; }
 
+        /// <summary>
+        ///     The text extra info in the notification.  Can
+        ///     be blank not supplied in the notification.
+        /// </summary>
         public string ExtraInfoText { get; set; }
     }
 }
--- Source/ExampleClient/ExampleClient/NotificationListener.cs
+++ Source/ExampleClient/ExampleClient/NotificationListener.cs
@@ -3,15 +3,25 @@ using Xamarin.Forms;
 
 namespace ExampleClient
 {
+    /// <summary>
+    /// Handle notifications.
+    /// </summary>
     public class NotificationListener : INotificationListener
     {
         /// <inheritdoc />
+        /// <remarks>
+        /// Parse out the 
+        /// </remarks>
         public void Recieved(Notification notification)
         {
-            var viewModel = new NotificationDisplayViewModel();
-            viewModel.Title = notification.Title;
-            viewModel.Message = notification.Message;
+            // Copy the notification values to the view model
+            var viewModel = new NotificationDisplayViewModel
+            {
+                Title = notification.Title,
+                Message = notification.Message
+            };
 
+            // Only copy the extra info if it exists.
             if (notification.ExtraInfo.ContainsKey("Number"))
             {
                 viewModel.ExtraInfoNumber = (int) notification.ExtraInfo["Number"];
@@ -22,8 +32,7 @@ namespace ExampleClient
                 viewModel.ExtraInfoText = (string) notification.ExtraInfo["Text"];
             }
 
-
-
+            // Show the notifcation page.
             var notificationPage = new NotificationDisplayPage(viewModel);
             Application.Current.MainPage.Navigation.PushAsync(notificationPage);
         }
